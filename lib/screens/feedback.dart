9c8e95f47cf91faaacf14600ae5a70ed2c013e1a
Sandra Effert <91104726+smefft@users.noreply.github.com>
2025-10-27 22:38:08 -0400

    Add static feedback widget

--- lib/screens/feedback.dart
+++ lib/screens/feedback.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:team_3_f25_project/widgets/custom_app_bar.dart';
+import 'package:team_3_f25_project/widgets/feedback_card.dart';
 
 class FeedbackScreen extends StatelessWidget {
   const FeedbackScreen({super.key});
@@ -8,6 +9,13 @@ class FeedbackScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context: context, title: "Feedback"),
+      body: Center(
+        child: FeedbackCard(
+          score: 100,
+          summaryFeedback: "Great!",
+          tips: ["Keep practicing"],
+        ),
+      ),
       bottomNavigationBar: Container(
         padding: EdgeInsets.all(10),
         margin: EdgeInsets.all(10),
