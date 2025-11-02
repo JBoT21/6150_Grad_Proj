9ff012388458b9b919b6af3cc498d1a6460d2789
Danny Ho <DannyHo0127@gmail.com>
2025-11-02 11:50:57 -0500

    Change database name

--- lib/services/user_db.dart
+++ lib/services/user_db.dart
@@ -10,7 +10,7 @@ class DatabaseHelper {
 
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDB('readright.db');
+    _database = await _initDB('readright_user.db');
     return _database!;
   }
 
