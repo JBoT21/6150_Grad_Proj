b289c393b043ff2a8082cdaca881ab80f9b5c437
Sandra Effert <91104726+smefft@users.noreply.github.com>
2025-11-03 21:33:05 -0500

    Remove logout button from app bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
 
 AppBar customAppBar({dynamic context, String title = ""}) {
-  return AppBar(
-    title: Text(title),
-    actions: [
-      ElevatedButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/');
-        },
-        child: Text("Logout"),
-      ),
-    ],
-  );
+  return AppBar(title: Text(title));
 }
