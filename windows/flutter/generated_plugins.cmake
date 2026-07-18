ba44d4eed9fc987ad2e191649471e3213a7bad72
luke8100 <lukecorbinsmith@charter.net>
2026-07-18 13:05:35 -0400

    FireBase Setup Complete
    Finished IOS FireBase Setup - firebase is now configured with ios and andriod. Test Pings write to the db

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,9 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   audioplayers_windows
+  cloud_firestore
   connectivity_plus
+  firebase_core
   flutter_tts
   permission_handler_windows
   record_windows
